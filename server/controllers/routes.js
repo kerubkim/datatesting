0c7d0a0a847f65d8d07847722d6b4813a377885c
kerubkim <kerubkimq@gmail.com>
2017-06-20 10:29:39 -0700

    Added routes & functionality for adding a user  and retrieving all users

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -12,9 +12,29 @@ module.exports = (function() {
         index: function(req, res) {
           //return that data in res.json()
           res.json({message: "This is a dummy data.",reason:"For testing purposes."});
-
         }
-
+        users: function(req,res){
+          // retrieving all users
+          User.find({}).exec(function(err,data){
+            if(err){
+              res.status(400).send("Error retrieving all users");
+            }else{
+              res.json(data);
+            }
+          });
+        }
+        newuser: function(req,res){
+          // creating user object
+          var user = new User({name:req.body.name,username:req.body.username,password:req.body.password,profession:req.body.profession,description:req.body.description});
+          // saving new user object
+          user.save(function(err,data){
+            if(err){
+              res.status(400).send("Error creating a new user");
+            }else{
+              res.sendStatus(200);
+            }
+          })
+        }
 
 
 
